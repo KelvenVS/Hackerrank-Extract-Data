0d4aec589eaf261ecbef004ca15b501120a48ce5
KelvenVS <kelven.serejo@gmail.com>
2025-03-02 18:25:57 -0300

    🐛 fix: corrigido caminho para execução dentro da pasta hackerrank

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-import json
+import json , os
 
 def open_json(file_location):
-    with open(file_location, 'r') as file:
+    
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(base_dir, file_location)
+    
+    with open(full_path, 'r') as file:
         data = json.load(file)
     return data
 
@@ -15,12 +19,10 @@ if __name__ == '__main__':
     # Itera sobre as submissões e imprime as informações
     for submission in submissions:
         score = submission.get('score')
-        
-        if score == 1:
+        if score >= 1:
             contest = submission.get('contest')
             challenge = submission.get('challenge')
             code = submission.get('code')
-            
             language = submission.get('language')
             
             print(f"Contest: {contest}")
@@ -28,4 +30,4 @@ if __name__ == '__main__':
             print(f"Code:\n {code}")
             print(f"Score: {score}")
             print(f"Language: {language}")
-            print("-" * 20)  # Separador entre submissões
+            print("-" * 50)  # Separador entre submissões
